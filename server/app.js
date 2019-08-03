aee6784af38dbf6264d87f4a9d7536abb029a53c
Londy Martina <londymartina@gmail.com>
2019-08-03 15:53:46 -0400

    Mongoose Setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { connect } from "mongoose";
+
+import { MONGO_DB_URL } from "./config"
 import routes from "./api"
+
 const app = express();
 
 app.use(routes());
-app.listen(3001, () => {
-    console.log("SERVER RUNNNG ON PORT 3001")
+connect(MONGO_DB_URL, { useNewUrlParser: true }, (err) => {
+    if(err) throw err
+    
+    app.listen(3002, () => {
+        console.log("SERVER RUNNNG O PORT 3002")
+    })
 })
---
+++ server/config/index.js
@@ -0,0 +1,3 @@
+export const MONGO_DB_NAME = "WeSchool";
+export const MONGO_DB_URL = `mongodb://127.0.0.1/${MONGO_DB_NAME}`;
+
